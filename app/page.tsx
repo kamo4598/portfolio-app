527e72c95e01aded3a52116e3733d8afd699103a
Hassan Kamran <hassankamran98@yahoo.com>
2023-11-21 18:33:15 +0300

    Test

--- app/page.tsx
+++ app/page.tsx
@@ -7,15 +7,15 @@ import Projects from "@/components/Projects";
 import Contact from "@/components/Contact";
 import ParticlesBackground from "@/components/particles/ParticlesBackground";
 import AboutMe from "@/components/AboutMe";
-import Sidebar from "@/components/sidebar/Sidebar";
+// import Sidebar from "@/components/sidebar/Sidebar";
 
 export default function Home() {
   return (
     <div className="flex h-auto max-w-[100vw] font-sans">
       <ParticlesBackground />
-      <div className="hidden lg:block">
+      {/* <div className="hidden lg:block">
         <Sidebar />
-      </div>
+      </div> */}
       <div className="flex-1 flex flex-col items-center justify-center">
         <div className="flex-1 min-h-[100vh] flex flex-col items-center justify-center">
           <Cover />
